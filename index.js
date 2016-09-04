a82e4b32110450b4f816fa34690655e11cdc69fa
Corey Flynn <flynn.corey@gmail.com>
2016-09-04 18:30:50 -0400

    Basic logging

---
+++ index.js
@@ -0,0 +1,24 @@
+var winston = require('winston');
+var express = require('express');
+var app = express();
+
+
+// pull in body parser so we can respond with json when a client posts to our API
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+
+// use pug to render views
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.post('/track', function (req, res) {
+  winston.info('Somebody Pushed It!', req.body);
+  res.send('You Pushed It!');
+});
+
+app.listen(3000, function () {
+  winston.info('PushIt is listening on port 3000!');
+});
