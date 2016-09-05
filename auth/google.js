af7056dd3f1bb91b3b3f1138b2ede0f9db4aaa55
Corey Flynn <flynn.corey@gmail.com>
2016-09-04 23:59:55 -0400

    Removes auth callback function

--- auth/google.js
+++ auth/google.js
@@ -9,10 +9,5 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "https://secure-fjord-65824.herokuapp.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
   }
 ));
